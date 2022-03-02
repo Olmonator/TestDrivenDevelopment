311a761c8701caa38acefd20d1d6dbf8f6253f70
Olmonator <olmo.lecht@posteo.de>
2022-03-02 17:07:06 +0200

    1 tick + only one falling Block implemented

--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -2,19 +2,19 @@ export class Board {
   width;
   height;
 
-  falling;
+  falling = false;
+  fallingRow = 0;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
-    this.falling = false;
   }
 
   toString() {
     let boardString = ''
     for (let row = 0; row < this.height; row++) {
       for (let col = 0; col < this.width; col++) {
-        if (row === 0 && col === 1 && this.falling) {
+        if (row === this.fallingRow && col === 1 && this.falling) {
           boardString += 'X';
         } else {
           boardString += '.';
@@ -25,7 +25,17 @@ export class Board {
     return boardString;
   }
 
+  // set a boolean that is checked to write X to Board
   drop() {
+    // check if Block is already falling
+    if (this.falling) {
+      throw new Error('already falling');
+    }
     this.falling = true;
+  }
+
+  // set an int to signal what row the Block is on
+  tick() {
+    this.fallingRow ++;
   }
 }
