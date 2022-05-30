da582eb6ddf13e7704461f439155ba729bbb0ab5
Olmo Lecht <olmo@pop-os.localdomain>
2022-05-30 19:48:20 +0300

    completion

--- GameOfLife/src/gameOfLife.mjs
+++ GameOfLife/src/gameOfLife.mjs
@@ -1,5 +1,4 @@
-import { create } from 'domain';
-import { readFileSync, unwatchFile } from 'fs';
+import { readFileSync } from 'fs';
 
 export function readFile(fileName) {
   try {
@@ -7,7 +6,7 @@ export function readFile(fileName) {
     //console.log(data);
     return data;
   } catch (err) {
-    console.error(err);
+    return false;
   }
 }
 
@@ -134,3 +133,198 @@ export function readRLE(fileName) {
 
   return patternToArray(arr, pattern, x, y);
 }
+
+function extendArray(arr) {
+  let array = arr;
+  let array2D;
+
+  // if 1D array, then turn into 2D
+  // e.g.       bbb
+  //      ooo   ooo
+  //            bbb
+  if(array[0].length === 1) {
+    array2D = createArray(array.length, array.length);
+    for (let x = 0; x < array.length; x++) {
+      for(let y = 0; y < array.length; y++) {
+        // only if in middle row 
+        if(x === 1) {
+          array2D[x][y] = array[x]; // array only 1D
+        } else {
+          array2D[x][y] = 'b';
+        }
+      }
+    }
+    array = array2D;
+  }
+
+  let extArr = createArray(array.length +2, array.length);
+  // fill up with dead cells
+  for(let x = 0; x < extArr.length; x++) {
+    for(let y = 0; y < extArr.length; y++) {
+      extArr[x][y] = 'b';
+    }
+  }
+
+  for(let x = 1; x < extArr.length -1; x++) {
+    for(let y = 1; y < extArr[0].length -1; y++) {
+      if(array[x-1][y-1] !== undefined) {
+        extArr[x][y] = array[x-1][y-1];
+      }
+    }
+  }
+
+  //console.log("array: ", extArr);
+  return extArr;
+}
+
+function check(arr, x, y, xMax, yMax) {
+  //check out of bounds
+  if(x >= 0 && x < xMax && y >= 0 && y < yMax) {
+    if (arr[x][y] === 'o') {
+      return true;
+    }
+  }
+  return false;
+}
+
+function getNeighbors(arr, x, y) {
+  let nOfAlive = 0;
+
+  // iterate over neighboring fields
+  for(let xOff = -1; xOff < 2; xOff++) {
+    for(let yOff = -1; yOff < 2; yOff++) {
+      // don't check element itself
+      if(xOff !== 0 || yOff !== 0) {
+        if(check(arr, x + xOff, y + yOff, arr.length, arr[0].length)) {
+          nOfAlive ++;
+        }
+      }
+    }
+  }
+
+  //console.log("alive neighboors: [" + x + "," + y + "] " + nOfAlive );
+  return nOfAlive;
+}
+
+export function iterate(fileName, iterations) {
+  // first extract the pattern
+  let arr = readRLE(fileName);
+  // somehow make space for the array | x=3 y=3 for more iterations
+  let extArr = extendArray(arr);
+  let resultArr;
+  //apply game logic
+  for(let i = 0; i < iterations; i++) {
+    resultArr = createArray(extArr.length, extArr[0].length);
+    for(let x = 0; x < extArr.length; x++) {
+      for (let y = 0; y < extArr[0].length; y++) {
+        // count neighbors
+        let neighbors = getNeighbors(extArr, x, y);
+        // rules
+        if(extArr[x][y] === 'o' && (neighbors === 2 || neighbors === 3)) {
+          resultArr[x][y] = 'o';
+        } else if (extArr[x][y] === 'b' && neighbors === 3 ) {
+          resultArr[x][y] = 'o';
+        } else {
+          resultArr[x][y] = 'b';
+        }
+      }
+    }
+    // overwrite arr to start again
+    extArr = resultArr;
+  }
+
+  return resultArr;
+}
+
+// can only delete horizontal line, if no alive cell appears
+function trimYAxis(arr, y) {
+  for(let x = 0; x < arr.length; x++) {
+    if(arr[x][y] === 'o') {
+      return false;
+    }
+  }
+  return true;
+}
+function trimArray(arr) {
+  // to counteract the deletion of lines, we need to subtract the amount of lines we have deleted
+  //  0 abc            0 abc
+  //  1 abc -> delete  
+  //  2 abc            1 abc
+  let slices = 0;
+  // copy array to work on
+  let array = createArray(arr.length, arr.length);
+  for (let x = 0; x < arr.length; x++) {
+    for (let y = 0; y < arr.length; y++) {
+      array[x][y] = arr[x][y];
+    }
+  }
+
+  for(let x = 0; x < arr.length; x++) {
+    //console.log("line: ", arr[x]);
+    if(!arr[x].includes('o')) {
+      array.splice(x - slices, 1);
+      slices ++;
+    } else {
+      for (let y = 0; y < arr.length; y++) {
+        if(trimYAxis(arr, y) === true) {
+          array[x -slices][y] = '';
+        }
+      }
+    }
+    //console.log("after", array);
+  }
+  
+  return array;
+}
+
+export function arrayToRLE(array) {
+  let rlePattern = '';
+  console.log("before", array);
+
+  // only trim if 2D
+  if (array[0].length !== 1) {
+    array = trimArray(array);
+  }
+ 
+  // try to trim some dead cells
+  console.log("after", array);
+
+  for(let x = 0; x < array.length; x++) {
+    // 1D array
+    if(array[0].length === 1) {
+      rlePattern += array[x];
+    } else {
+      for (let y = 0; y < array[0].length; y++) {
+        rlePattern += array[x][y];
+      }
+      if(x < array.length -1) {
+        rlePattern += '$';
+      }
+    }
+    
+  }
+
+  rlePattern += '!';
+  return rlePattern;
+}
+
+export function handleCommandLine(args) {
+  console.log(typeof(args[0]));
+  if(args.length < 2) {
+    return "Error: Too few arguments, please give a path and the number of iterations to run!";
+  } else if (args.length > 2) {
+    return "Error: Too many arguments, please give a path and the number of iterations to run!";
+  } else if(typeof(args[0]) !== "string") {
+    return "Error: Path in wrong format, please provide a string";
+  } else if(readFile(args[0]) === false) {
+    return "Error: Path not found, please provide a correct path!";
+  } else if(Number.isNaN(parseInt(args[1]))) {
+    return "Error: Iterations in wrong format, please provide an integer!";
+  } else if(parseInt(args[1]) < 1) {
+    return "Error: Iterations are negative, only positive numbers allowed!";
+  } else {
+    return arrayToRLE(iterate(args[0], args[1]));
+  }
+}
+
+handleCommandLine(process.args.splice(2,2));
--- GameOfLife/test/gameOfLife.test.mjs
+++ GameOfLife/test/gameOfLife.test.mjs
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { readFile, readRLE, arrToString, iterate } from "../src/gameOfLife.mjs";
+import { readFile, readRLE, arrToString, iterate, arrayToRLE, handleCommandLine } from "../src/gameOfLife.mjs";
 
 describe("Read RLE Files", () => {
   it("Read Files", () => {
@@ -29,4 +29,94 @@ describe("Read RLE Files", () => {
       expect(arrToString(readRLE("src/testFiles/glider.rle"))).to.equal("bob\nbbo\nooo\n");
     });
   });
+});
+
+describe("Game Logic", () => {
+  describe("Blinker", () => {
+    it("One Iteration", () => {
+      expect(arrToString(iterate("src/testFiles/blinker.rle", 1))).to.equal("bbbbb\nbbobb\nbbobb\nbbobb\nbbbbb\n");
+    });
+    it("Two Iterations", () => {
+      expect(arrToString(iterate("src/testFiles/blinker.rle", 2))).to.equal("bbbbb\nbbbbb\nbooob\nbbbbb\nbbbbb\n");
+    });
+  });
+  describe("Block", () => {
+    it("One Iteration", () => {
+      expect(arrToString(iterate("src/testFiles/block.rle", 1))).to.equal("bbbb\nboob\nboob\nbbbb\n");
+    });
+  });
+  describe("Glider", () => {
+    it("One Iteration", () => {
+      // because the glider moves, need to extend the matrix
+      // bbo  bbbbb         bbbbb
+      // obo  bbbob iterate bbbbb
+      // boo  bobob         bobob
+      //      bboob         bboob
+      //      bbbbb         bbobb
+      expect(arrToString(iterate("src/testFiles/glider.rle", 1))).to.equal("bbbbb\nbbbbb\nbobob\nbboob\nbbobb\n");
+    });
+    it("Two Iterations", () => {
+      expect(arrToString(iterate("src/testFiles/glider.rle", 2))).to.equal("bbbbb\nbbbbb\nbbbob\nbobob\nbboob\n");
+    });
+    it("Three Iterations", () => {
+      expect(arrToString(iterate("src/testFiles/glider.rle", 3))).to.equal("bbbbb\nbbbbb\nbbobb\nbbboo\nbboob\n");
+    });
+  });
+});
+
+describe("Array To RLE format", () => {
+  it("Blinker - 0 iterations", () => {
+    expect(arrayToRLE(readRLE("src/testFiles/blinker.rle"))).to.equal("ooo!");
+  });
+  it("Blinker - 1 iteration", () => {
+    expect(arrayToRLE(iterate("src/testFiles/blinker.rle", 1))).to.equal("o$o$o!");
+  });
+  it("Block - 0 iterations", () => {
+    expect(arrayToRLE(readRLE("src/testFiles/block.rle"))).to.equal("oo$oo!");
+  });
+  it("Block - 1 iteration", () => {
+    expect(arrayToRLE(iterate("src/testFiles/block.rle", 1))).to.equal("oo$oo!");
+  });
+  it("Glider - 0 iterations", () => {
+    expect(arrayToRLE(readRLE("src/testFiles/glider.rle"))).to.equal("bob$bbo$ooo!");
+  });
+  it("Glider -  iteration", () => {
+    expect(arrayToRLE(iterate("src/testFiles/glider.rle", 1))).to.equal("obo$boo$bob!");
+  });
+});
+
+describe("Command Line", () => {
+  describe("Arguments", () => {
+    it("too few arguments", () => {
+      expect(handleCommandLine(["path"])).to.equal("Error: Too few arguments, please give a path and the number of iterations to run!");
+    });
+    it("too many arguments", () => {
+      expect(handleCommandLine(["path", 2, 32])).to.equal("Error: Too many arguments, please give a path and the number of iterations to run!");
+    });
+    describe("File Path", () => {
+      it("path is no string", () => {
+        expect(handleCommandLine([2, 2])).to.equal("Error: Path in wrong format, please provide a string");
+      });
+      it("path not found", () => {
+        expect(handleCommandLine(["path", 2])).to.equal("Error: Path not found, please provide a correct path!");
+      });
+      describe("Iterations", () => {
+        it("iterations is no int", () => {
+          expect(handleCommandLine(["src/testFiles/block.rle", "hello"])).to.equal("Error: Iterations in wrong format, please provide an integer!");
+        });
+        it("iterations are negative", () => {
+          expect(handleCommandLine(["src/testFiles/block.rle", -1])).to.equal("Error: Iterations are negative, only positive numbers allowed!");
+        });
+        describe("Functionality", () => {
+          it("blinker works", () => {
+            expect(handleCommandLine(["src/testFiles/blinker.rle", 1])).to.equal("o$o$o!");
+          });
+          it("glider works", () => {
+            expect(handleCommandLine(["src/testFiles/glider.rle", 2])).to.equal("bbo$obo$boo!");
+          });
+        })
+      });
+    });
+  });
+  
 });
